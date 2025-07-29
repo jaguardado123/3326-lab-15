78cb08389133911e9d25bfbb2cca61e20d41104e
Joselito A Guardado <36009999+jaguardado123@users.noreply.github.com>
2025-07-29 13:06:31 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 # Compile Java code
 javac -cp "./lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar:" -d ./bin ./src/*.java
 # Run test
-java -cp "./bin:./lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar:" org.junit.runner.JUnitCore MainTest
+java -cp "./bin:./lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar:" org.junit.runner.JUnitCore RecursiveTest
