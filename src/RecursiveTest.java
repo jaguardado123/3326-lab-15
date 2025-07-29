0a6b9eb13c6e4e87ffef05103baac592fb7145c9
Joselito A Guardado <36009999+jaguardado123@users.noreply.github.com>
2025-07-29 13:05:32 -0500

    Update and rename MainTest.java to RecursiveTest.java

---
+++ src/RecursiveTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class RecursiveTest {
+	
+	@Test
+	public void testing_pow() {
+		// 2^3 should equal 8
+		assertEquals(8, Recursive.pow(2,3));
+		// 3^4 should equal 81
+		assertEquals(81, Recursive.pow(3,4));
+		// 5^4 should equal 625
+		assertEquals(625, Recursive.pow(5,4));
+	}
+
+	@Test
+	public void testing_sumN() {
+		// sum of 1 to 3 should equal 6
+		assertEquals(6, Recursive.sumN(3));
+		// sum of 1 to 5 should equal 15
+		assertEquals(15, Recursive.sumN(5));
+		// sum of 1 to 7 should equal 28
+		assertEquals(28, Recursive.sumN(7));
+	}
+
+	@Test
+	public void testing_search() {
+		// searching for "Howdy" should return false
+		String[] words = {"hello", "hey", "sup" "oi"};
+		assertEquals(false, Recursive.search(words, "Howdy", 0));
+		// searching for "grapes" should return true
+		String[] foods = {"apple", "orange", "grapes" "bananas"};
+		assertEquals(true, Recursive.search(foods, "grapes", 0));
+		// searching for "hi" should return false
+		String[] empty = {};
+		assertEquals(false, Recursive.search(empty, "hi", 0));
+	}
+}
